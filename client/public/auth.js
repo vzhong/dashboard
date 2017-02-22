74ebfa24403e159111bf07ac52d752b383c351b4
Victor Zhong <victor@victorzhong.com>
2017-02-21 20:48:49 -0800

    auth with redirect instead of popup

--- client/public/auth.js
+++ client/public/auth.js
@@ -1,21 +1,25 @@
 function login() {
     var provider = new firebase.auth.GoogleAuthProvider();
-        firebase.auth().signInWithPopup(provider).then(function(result) {
+    firebase.auth().signInWithRedirect(provider);
+    firebase.auth().getRedirectResult().then(function(result) {
+        if (result.credential) {
             // This gives you a Google Access Token. You can use it to access the Google API.
             var token = result.credential.accessToken;
-            // The signed-in user info.
-            var user = result.user;
-            return user;
-        }).catch(function(error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // The email of the user's account used.
-            var email = error.email;
-            // The firebase.auth.AuthCredential type that was used.
-            var credential = error.credential;
-            console.log(errorMessage);
-        });
+            // ...
+        }
+        // The signed-in user info.
+        var user = result.user;
+    }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+        console.log(errorMessage);
+    });
 }
 
 function ensure_auth() {
