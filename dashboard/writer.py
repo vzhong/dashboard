60f7391d891dbe3fec0436ca8a6dc44665e2d830
Victor Zhong <victor@victorzhong.com>
2017-02-21 16:02:53 -0800

    add password auth to writer

--- dashboard/writer.py
+++ dashboard/writer.py
@@ -45,31 +45,25 @@ class FileWriter(Writer):
 
 class FirebaseWriter(Writer):
 
-    def __init__(self, name, api_key=None, auth_domain=None, database_url=None, storage_bucket=None, delete_existing=False):
+    def __init__(self, name, delete_existing=False):
         fconfig = os.path.join(os.environ['HOME'], '.fb.config')
-        config = {}
-        if os.path.isfile(fconfig):
-            with open(fconfig) as f:
-                config.update(json.load(f))
-        if api_key:
-            config['apiKey'] = api_key
-        if auth_domain:
-            config['authDomain'] = auth_domain
-        if database_url:
-            config['databaseURL'] = database_url
-        if storage_bucket:
-            config['storageBucket'] = storage_bucket
-        assert config, 'failed to load config! Pass in your config or make your config file at {}.'.format(fconfig)
+        assert os.path.isfile(fconfig), 'Failed to load config file from {}'.format(fconfig)
+        with open(fconfig) as f:
+            config = json.load(f)
+
         self.name = name
         self.fb = pyrebase.initialize_app(config)
+        self.user = self.fb.auth().sign_in_with_email_and_password(config['email'], config['password'])
         if delete_existing:
             self.delete()
 
     def delete(self):
-        self.fb.database().child('experiments').child(self.name).remove()
+        self.user = self.fb.auth().refresh(self.user['refreshToken'])
+        self.fb.database().child('experiments').child(self.name).remove(self.user['idToken'])
 
     def add(self, metrics):
-        self.fb.database().child('experiments').child(self.name).push(metrics)
+        self.user = self.fb.auth().refresh(self.user['refreshToken'])
+        self.fb.database().child('experiments').child(self.name).push(metrics, self.user['idToken'])
 
 
 if __name__ == '__main__':
